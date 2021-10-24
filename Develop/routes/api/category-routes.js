c369d99220dfdb46b84972858ca39140d509bc36
Tony Slonaker <tony.slonaker@gmail.com>
2021-10-24 16:20:46 -0600

    update api folder

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,15 +6,62 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: [
+      'category_name',
+      'id'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: ['category_id', 'id', 'price', 'product_name', 'stock'],
+      }
+    ]
+})
+
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(404).json(err);
+   });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id 
+    },
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Product, 
+        attributes: ['category_id', 'id', 'price', 'product_name', 'stock'],
+      }
+    ]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(404).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+    id: req.body.id
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(404).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
